95e0fe476ce9b89336bf97cc72bb15a781bcdd7a
rturcey <rturcey@student.42.fr>
2020-12-05 10:55:04 +0100

    test1

--- philo_three/states.c
+++ philo_three/states.c
@@ -6,7 +6,7 @@
 /*   By: rturcey <rturcey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 09:48:15 by rturcey           #+#    #+#             */
-/*   Updated: 2020/12/05 10:39:28 by rturcey          ###   ########.fr       */
+/*   Updated: 2020/12/05 10:54:35 by rturcey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@ int		check_death(t_phi *phi)
 	phi->time = time_ms() - phi->origin;
 	if (g_isdead == 0 && ((phi->time - phi->prev_meal) < phi->time_to_die))
 		return (0);
-	if (g_isdead == 0)
-		print_msg(ft_strdup("died\n"), phi);
-	g_isdead = 1;
 	return (1);
 }
 
